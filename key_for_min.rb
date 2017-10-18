55e317543e953d3286d3a1484f50e4808ad89f18
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-10-18 20:19:09 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.collect do |key, value|
-    if value name_hash[1] > value name_hash[2] 
-      return name_hash[1] key
+  name_hash.collect do |name, age|
+    if age[0] < age[1] && age[0] < age[2]
+      return name[0]
+    elsif age[1] < age[0] && age[1] < age[2]
+      return name[1]
+    elsif age[2] < age[0] && age[2] < age[1]
+      return name[2]
+    end
 end
